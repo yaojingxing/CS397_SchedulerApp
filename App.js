30d635a2f1733515fd72716c08179c552a9bebd0
yaojingxing <yaojingxing@yaojingxingdeMacBook-Pro.local>
2020-09-20 01:06:15 -0500

    start up

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start fftt1122</Text>
+      <Text>Welcome to my App!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +14,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor:'blue',
     alignItems: 'center',
     justifyContent: 'center',
   },
